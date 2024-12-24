a384de69d516d53c1b77686decaa57f2f045ced6
shishirghimiree <shishirghimirecsit077@indrenicollege.edu.np>
2024-12-24 14:58:27 +0545

    Lazarev

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+
+  // for tablet smooth
+  tablet: { smooth: true },
+
+  // for mobile
+  smartphone: { smooth: true }
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+locomotiveAnimation()
+
+function loadingAnimation(){
+    var tl=gsap.timeline()
+    tl.from("#page1",{
+        opacity:0,
+        duration:0.2,
+        delay:0.2
+    })
+    tl.from("#page1",{
+        transform:"scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius:"100px",
+        duration:2,
+        ease:"expo.out"
+    })
+    tl.from("nav",{
+        opacity:0,
+        delay: -0.2
+    })
+    tl.from("#page1 h1,#page1 p,#page1 div",{
+        opacity:0,
+        duration:0.5,
+        stagger:0.2
+    })
+}
+loadingAnimation()
+
 function navAnimation(){
     var nav =document.querySelector("nav")
 
@@ -61,7 +124,7 @@ rightElem.forEach(function(elem){
     })
 })
 }
-// page2Animation()
+page2Animation()
 
 function page3VideoAnimation() {
     var page3Center = document.querySelector("#page-3-center")
@@ -83,9 +146,6 @@ video.addEventListener("click",function(){
         borderRadius:"30px"
     })
 })
-}
-// page3VideoAnimation()
-
 var sections =document.querySelectorAll("#sec-right")
 sections.forEach(function(elem){
         elem.addEventListener("mouseenter",function(){
@@ -95,6 +155,22 @@ sections.forEach(function(elem){
     elem.addEventListener("mouseleave",function(){
         elem.childNodes[3].style.opacity=0
         elem.childNodes[3].load()
+}) 
 })
-    
-})
+}
+page3VideoAnimation()
+
+function scrollAnimation(){
+    gsap.from("#page7-part2 h4",{
+        x:0,
+        duration:1,
+        scrollTrigger:{
+            trigger:"#page7-part2",
+            scroller:"#main",
+            start:"top 80%",
+            end:"bottom 10%",
+            scrub: true,
+        }
+    })
+}
+scrollAnimation()
